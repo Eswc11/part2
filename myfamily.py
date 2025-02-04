a6f674d185532f0639aed1f2bd9817a835ea3895
Eswc11 <151701840+Eswc11@users.noreply.github.com>
2025-02-04 11:46:41 +0600

    Add files via upload

---
+++ myfamily.py
@@ -0,0 +1,14 @@
+myfamily = ("mother", "father", "sister", "brother", "sister") 
+print(type(myfamily)) 
+
+
+myfamily = ("mother", "father", "sister", "brother", "sister") 
+print(myfamily[2]) 
+
+myfamily = ("mother", "father", "sister", "brother", "sister") 
+myfamily.append = "me"
+print(myfamily)
+
+myfamily = ("mother", "father", "sister", "brother", "sister") 
+print(myfamily.pop(3))
+
